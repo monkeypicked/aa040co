4d06005c1785b33811e1a8aa816bace7d74e9a51
unknown <Bloomberg@SILVER-8.silverstreet.lan>
2015-01-09 14:52:37 +0000

    aatest runs ok

--- tests/aa040cotest.R
+++ tests/aa040cotest.R
@@ -1,6 +1,7 @@
 require(aautil)
 require(aaco)
-require(aa)
+#require(aa)
+require(quadprog)
 aatopselect("test")
 
 
@@ -10,21 +11,20 @@ aatopselect("test")
 
 da <- c("2014-10-22","2014-10-29")
 bui <- buiindirs()
-su.g <- setkey(setnames(data.table(expand.grid(da,bui)),c('date','bui')),date,bui)[]
+su.g <<- setkey(setnames(data.table(expand.grid(da,bui)),c('date','bui')),date,bui)[]
 
 ce <- rbindlist(lapply(lapply(da,ceload,nfac=1),data.table))
-
 expect_equal(ce[,sort(unique(date))],sort(unique(da)))
 
 #slower bit
-if(F) {
-  sfInit(par=FALSE)
-  system.time(xx<-rbindlist(lapply(sfLapplyWrap(dax,ceload),data.table)))
-  sfStop()
-  xx[,unique(date)]
-
-  sfInit(par=TRUE,cpus=5)
-  system.time(xx<-rbindlist(lapply(sfLapplyWrap(dax,ceload),data.table)))
-  #sfStop()
-  putrd(xx,"ce 20 final dates")
-}
+# if(FALSE) {
+#   sfInit(par=FALSE)
+#   system.time(xx<-rbindlist(lapply(sfLapplyWrap(dax,ceload),data.table)))
+#   sfStop()
+#   xx[,unique(date)]
+# 
+#   sfInit(par=TRUE,cpus=5)
+#   system.time(xx<-rbindlist(lapply(sfLapplyWrap(dax,ceload),data.table)))
+#   #sfStop()
+#   putrd(xx,"ce 20 final dates")
+# }
